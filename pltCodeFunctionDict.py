7e193fc5d03323db1b20a6107bc955549ed01228
Magicxiangye <30616925+Magicxiangye@users.noreply.github.com>
2020-12-10 22:06:32 +0800

    matplotlibTestCodeDict

--- pltCodeFunctionDict.py
+++ pltCodeFunctionDict.py
@@ -1,6 +1,7 @@
 #可视化数据库matplotlib的代码实验
 from collections import Counter
 
+
 from matplotlib import pyplot as plt
 
 
@@ -36,10 +37,58 @@ def lineBar():
 
     plt.show()
 
+
+#线性图
+def labLine():
+    variance = [1,2,4,8,16,32,64,128,256]
+    bias_squared = [256,128,64,32,16,8,4,2,1]
+    total_error = [x+y for x,y in zip(variance,bias_squared)]
+    xs = [i for i,_ in enumerate(variance)]#x轴的数据
+
+    #多次调用plt.plot()来在同一张的图上画不同的线
+    plt.plot(xs, variance,'g-', label='variance')
+    plt.plot(xs, bias_squared, 'r-', label='bias_squared')
+    plt.plot(xs, total_error, 'b:', label='total_error')
+
+    #图中各个标识的设置(中文的显示还是有点问题)
+    plt.legend(loc=9)#设置的是图中各个label的摆放的位置
+    plt.xlabel("模型的复杂度")
+    plt.title('test number')
+    plt.show()
+
+
+#散点图
+def scatterChart():
+    #成对数据集的可视化的好选择
+    friend =  [70, 65, 72, 63, 71, 64, 60, 64, 67]
+    minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+    labels =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+    #使用的画图的工具
+    plt.scatter(friend,minutes)
+
+    #散点图的每个对应点的标记
+    #还是会用到压缩的函数zip()
+    for label, friend_count,minute_count in zip(labels,friend,minutes):
+        #每一个点标记的设定
+        #xy为被注释的坐标点xytext为注释文字的坐标位置(textcoords有多种的参数)
+        plt.annotate(label, xy=(friend_count,minute_count), xytext=(-5,5), textcoords='offset points')
+
+    # 出图前最后的标记
+    #plt.axis()可以设置带有比较轴的散点图
+    plt.title('friends and play munite relation')
+    plt.xlabel('friend_count')
+    plt.ylabel('play times')
+    plt.show()
+
+
+
 if __name__ == '__main__':
-    grades = [77,54,78,89,90,93,97]
-    decile = lambda grade: (grade//10) * 10
-    histest = Counter(decile(grade) for grade in grades)
-    print(histest)
+    # grades = [77,54,78,89,90,93,97]
+    # decile = lambda grade: (grade//10) * 10
+    # histest = Counter(decile(grade) for grade in grades)
+    # print(histest)
+    #labLine()
+    scatterChart()
 
 
