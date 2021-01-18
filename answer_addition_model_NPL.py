15741facda35f09b2ac2568b09fb4377fff82dee
Magicxiangye <30616925+Magicxiangye@users.noreply.github.com>
2021-01-18 22:39:52 +0800

    NPL_answerModel_code

--- answer_addition_model_NPL.py
+++ answer_addition_model_NPL.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 import numpy as np
 
-#回答加法计算的回答神经网络
-#生成随机数的函数
-#digits默认设置的是最多生成三位数的数
+# 回答加法计算的回答神经网络
+# 生成随机数的函数
+# digits默认设置的是最多生成三位数的数
 def n(digits=3):
-    #先拿来存放每一位的随机生成的数
-    #最后再用函数来转换为int型
+    # 先拿来存放每一位的随机生成的数
+    # 最后再用函数来转换为int型
     number = ''
     # 是从序列中获取一个随机元素
     for i in range(np.random.randint(1, digits + 1)):
         number += np.random.choice(list('0123456789'))
-    #转换类型返回数值
+    # 转换类型返回数值
     return int(number)
 
 
